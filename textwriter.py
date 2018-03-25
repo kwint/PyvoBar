ba07f3ae84764cea86615e81f70ac13ba0bf8469
Quinty van Dijk <quintvdijk@gmail.com>
2018-03-25 20:50:20 +0200

    Don't send emails when balance is 0

--- textwriter.py
+++ textwriter.py
@@ -3,7 +3,7 @@ def make(name, balance):
     balance_float = float(balance.replace(",",".").replace("€", "").replace(" ", ""))
     print(balance_float)
     if balance_float < 0.0:
-        personal_message = "Op het moment is je saldo negatief, dat betekent dat je de bar geld verschuildigd bent.<br>" \
+        personal_message = "Op het moment is je saldo negatief, dat betekent dat je de bar geld verschuldigd bent.<br>" \
                            "We willen graag dat men een positief saldo heeft, zodat we geen leningen uitgeven. " \
                            "We verzoeken je zo snel mogelijk het verschuldigde bedrag naar ons over te maken. "
     elif balance_float >= 0.0:
@@ -15,13 +15,16 @@ def make(name, balance):
               <head></head>
               <body>
                 <p>Beste {name},<br><br>
+                                
                 Turfjes zijn weer verwerkt en je saldo is geüpdatet!<br>
                 Op het moment is je saldo: <b>{balance}</b><br><br>
                 {personal_message}<br><br>
                 
-                Het rekeningnummer voor het opwaarderen is: NL92 INGB 0004806266 t.n.v. 'M Jeremic en/of Q van Dijk<br>
+                Het rekeningnummer voor het opwaarderen is: NL92 INGB 0004806266 t.n.v. 'M Jeremic en/of Q van Dijk, onder vermelding van je voornaam.<br>
                 De digitale barlijst kan je <a href="https://drive.google.com/drive/folders/0B-xczYurTophWE0yRVFjWi1wNWM?usp=sharing">hier</a> vinden. Als je denkt dat we een foutje hebben 
-                gemaakt neem dan even contact op!<br><br>
+                gemaakt neem dan even contact op!<br>
+                
+                Geen mails meer ontvangen? Zorg er voor dat je saldo precies 0.00 euro is, je ontvangt dan automatisch geen mails meer.<br>
                 
                 Met vriendelijke groeten,<br><br><br>
                 
